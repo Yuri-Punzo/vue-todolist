328d9764ef14b3a0badd7c247e864b8e04299869
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-28 17:10:23 +0200

    progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,46 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            error: false,
+            newTask: {
+                text: "",
+                done: false,
+            },
+            tasks: [
+                {
+                    text: "spazzare",
+                    done: false,
+                },
+                {
+                    text: "spolverare",
+                    done: false,
+                },
+                {
+                    text: "passare lo straccio",
+                    done: false,
+                }
+            ]
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.text.length < 5) {
+                this.error = true
+            } else {
+                this.tasks.unshift(this.newTask)
+                this.newTask = ""
+                this.error = false
+            }
+        },
+        submit() {
+            this.addTask()
+        },
+        taskDone(i) {
+            console.log("clicked", i);
+            this.tasks[i].done = !this.tasks[i].done;
+        },
+        removeTask(i) {
+            this.tasks.splice(i, 1)
         }
     }
 }).mount('#app')
